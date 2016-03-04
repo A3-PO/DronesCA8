e908b43a74705125862ee530d1476b2693db991a
alvaro <aperez15@student.aau.dk>
2016-03-04 10:45:32 +0100

    Some scenario m file changes

--- src/matlab/Scenario_simulation_2D.m
+++ src/matlab/Scenario_simulation_2D.m
@@ -1,9 +1,16 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%**************************************************************************
 %
-%                       Main File for the Project
+% CA8 - DRONES
 %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+%**************************************************************************
+%
+% Group 832 - Control and Automation Msc.
+% Aalborg University
+% February 2016
+%
+%**************************************************************************
+%
+% Description:
 % In order to understant better how it works the supervisor told us that
 % cold be important to construct the graph of the antenna intensity 
 % radiation.
@@ -30,40 +37,31 @@ y_drone = 50; % The position y of the ground station
 x_drone = -10; % The position x of the ground station
 height = 0:step_h:y_drone;
 
-figure(1)
-plot(x_drone,y_drone,'o','LineWidth',2);
-title('Simulation of the comunication between the drone and the Ground Station');
-hold on
-plot(x_gs,y_gs,'X','LineWidth',2);
-legend('Drone','Ground Station');
+% Small function to draw arrows
+drawArrow = @(x,y) quiver( x(1),y(1),x(2)-x(1),y(2)-y(1),0,'LineWidth',2.5,'MaxHeadSize',0.5);  
 
 %% Ground Station definition
 
-%angle_gs = 2*pi/3;
-angle_gs = atan(abs(x_gs-x_drone)/abs(y_drone-y_gs)) + pi/2;
-x_start = x_gs;
-y_start = y_gs;
+angle_gs = 2*pi/3;
+% angle_gs = atan(abs(x_gs-x_drone)/abs(y_drone-y_gs)) + pi/2;
+x_start_gs = x_gs;
+y_start_gs = y_gs;
 
-x_end = x_gs + x_gs/5*cos(angle_gs);
-y_end = y_gs + x_gs/5*sin(angle_gs);
-
-hold on
-line([x_start,x_end],[y_start,y_end]);
-
+x_end_gs = x_gs + x_gs/5*cos(angle_gs);
+y_end_gs = y_gs + x_gs/5*sin(angle_gs);
 
 %% Drone definition
 
-%angle_d = 3*pi/2 + pi/9;
-angle_d = atan(abs(x_gs-x_drone)/abs(y_drone-y_gs)) + 3/2*pi;
+angle_d = 3*pi/2 + pi/9;
+% angle_d = atan(abs(x_gs-x_drone)/abs(y_drone-y_gs)) + 3/2*pi;
 
-x_start = x_drone;
-y_start = y_drone;
+x_start_d = x_drone;
+y_start_d = y_drone;
 
-x_end = x_drone + y_drone/5*cos(angle_d);
-y_end = y_drone + y_drone/5*sin(angle_d);
+x_end_d = x_drone + y_drone/5*cos(angle_d);
+y_end_d = y_drone + y_drone/5*sin(angle_d);
 
-hold on
-line([x_start,x_end],[y_start,y_end]);
+
 
 %% Connection between two origins
 
@@ -90,28 +88,32 @@ end
 
 y = m*d + b;
 
-hold on
-plot(d,y);
-
-
 %% Calculation 
 
 beta = atan(abs(y_drone-y_gs)/abs(x_gs-x_drone));
-
 phi_gs = pi - angle_gs - beta;
-
 phi_gs = phi_gs*180/pi;
-
 phi_d = 2*pi-(beta + angle_d);
-
 phi_d = phi_d*180/pi;
 
+[GSgain,angle3db_gs] = GSantenna(phi_gs,0);
+[Dgain,angle3db_d] = GSantenna(phi_d,0);
 
-[GSgain,angle3db] = GSantenna(phi_gs,1);
+% Representation
 
-[Dgain,angle3db] = GSantenna(phi_d,0);
-
-
-
+figure();
+hold on
+plot(x_drone,y_drone,'o','LineWidth',2);
+plot(x_gs,y_gs,'X','LineWidth',2);
+drawArrow([x_start_gs,x_end_gs],[y_start_gs,y_end_gs]);
+drawArrow([x_start_d,x_end_d],[y_start_d,y_end_d]);
+plot(d,y,'LineWidth',1.5);
+grid on;
+grid minor;
+str = sprintf('Scenario Simulation 2D \n GS Antenna Gain: %.3f dB \n Drone Antenna Gain: %.3f dB',GSgain,Dgain);
+title(str);
+xlabel('X world axis');
+ylabel('Y world axis');
+legend('Drone','Ground Station','GS Frame','Drone Frame','LOS distance');
 
 

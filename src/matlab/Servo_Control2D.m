155357ace40146a521f23efa793a2155a7c5b9fd
Kenny <klafon15@student.aau.dk>
2016-04-05 10:18:23 +0200

    2D controller

---
+++ src/matlab/Servo_Control2D.m
@@ -0,0 +1,25 @@
+%**************************************************************************
+%               2D controller for DC servo motor
+%**************************************************************************
+
+clear all; close all;
+
+% Motor parameters
+Ra  = 2.6;               % Resistor [Ohm]
+La  = 180*10^(-6);       % Inductance [H]
+Kt  = 7.67*10^(-3);      % Torque constant [N*m/A]
+Kb  = 7.67*10^(-3);      % Back-emf constant [V/(r/s)]
+Je  = 5.3*10^(-7);       % Load attached to motor shaft [kg*m^2]
+De  = 7.7*10^(-6);       % Damping behavior [N*m/(r/s)]
+
+%GI  = 1/(Ra + La*s)
+%GO  = 1/(Je*s^2 + De*s)
+
+
+% PID parameters
+% The 'Good gain method' has been used to tune the PID
+Kp  =  0.3
+
+Ti  = 0.2 %Kp/Ki
+Td  = Ti/4 %Kd/Kp
+

ec0f776c42fbc7ae6825cb2302cd2b677c87d046
Robert <popescu.robert.octavian@gmail.com>
2016-04-06 09:01:47 +0200

    m

--- src/matlab/los_coverage_map.m
+++ src/matlab/los_coverage_map.m
@@ -2,9 +2,10 @@
 clear all; close all; clc;
 % Input parameters
 h_g = input('Input ground station altitude: ');   % [m] station altitude 
-h_d = input('Input ground station altitude: ');   % [m] drone altitude
+h_d = input('Input drone altitude: ');            % [m] drone altitude
 
 %% 1. Import map as a Web Map Service (WMS)
+% Denmark latitudes and longitudes
 latDK = [53 58];
 lonDK = [8 13];
 layers = wmsfind('esa.int', 'SearchField', 'serverurl');
@@ -15,19 +16,19 @@ gtopo30Layer.Lonlim = lonDK;
 oceanColor = [0 170 255];
 [A, R] = wmsread(gtopo30Layer, 'BackgroundColor', oceanColor);
 
-%% 2. LOS from ground station to drone
+%% 2. LOS distance from ground station to drone (2 points)
 figure
 worldmap(latDK, lonDK)
 geoshow(A, R, 'DisplayType', 'texturemap')
 title({'GTOPO30 Elevation Model',gtopo30Layer.LayerTitle})
-[lat lon] = inputm(2);  % input 2 points on map to get lat and lon
-Z = double(A(:,:,1));   % taking one layer of the map
+[lat lon] = inputm(2);      % input 2 points on map to get lat and lon
+Z = double(A(:,:,1));       % map layer
 e_g = ltln2val(Z, R, lat(1), lon(1)) + h_g; % [m] terrain + station height
 e_d = ltln2val(Z, R, lat(2), lon(2)) + h_d; % [m] terrain + drone height
-D = 3.57*(sqrt(e_g) + sqrt(e_d))            % [km] horizon (LOS) distance 
+D = 3.57*(sqrt(e_g) + sqrt(e_d)) % [km] theoretical horizon (LOS) distance 
 los2(Z, R, lat(1), lon(1), lat(2), lon(2), h_g, h_d)
 
-%% 3. LOS coverage map
+%% 3. LOS coverage map (Area)
 figure
 worldmap(latDK, lonDK)
 geoshow(A, R, 'DisplayType', 'texturemap')
@@ -43,6 +44,6 @@ end
 map = A + vis;
 geoshow(map, R, 'DisplayType', 'texturemap')
 title({'LOS COVERAGE MAP',gtopo30Layer.LayerTitle})
-plotm(latG,lonG,'bo');
+plotm(latG,lonG,'bo')
 
 
